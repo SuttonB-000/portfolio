fbf44a0f410cb8d525d50d2226984ef17b1c6a73
SuttonB-000 <SuttonB-000@protonmail.com>
2026-02-20 21:31:28 -0500

    Fleshed out the logic for the drop down menu, changed some css details for menu as well

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 //hamburger menu
-let menuButton = document.getElementById("")
+//identify button
+let menuButton = document.getElementById("menu"); 
+
+//function of what I want to do 
+function menuDrop() {
+    var menuContent = document.getElementById("menuContent");
+    menuContent.style.display = "block";
+}
+
+//add listener  to identify menu activation
+menuButton.addEventListener("click", menuDrop);
